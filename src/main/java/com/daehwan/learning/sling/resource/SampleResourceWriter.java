1756f8b781f884edc2ba6b6625b5b06d016302d2
daehwan <daehwann@gmail.com>
2015-09-17 11:35:06 +0900

    service user session 조회 수정.

--- src/main/java/com/daehwan/learning/sling/resource/SampleResourceWriter.java
+++ src/main/java/com/daehwan/learning/sling/resource/SampleResourceWriter.java
@@ -33,20 +33,16 @@ public class SampleResourceWriter{
     private ResourceResolverFactory resolverFactory;
  
     @Activate
-    public void doAWriteOperation(ComponentContext ctx) {
+    public void doAWriteOperat£ion(ComponentContext ctx) {
     	/**
     	 * ResourceResolver는 각각의 서비스 별로 다른 권한을 가진다.
     	 * 서비스의 권한은 매핑된 유저의 권한을 가진다.
     	 * 
     	 * ‘serviceName [ ":" subServiceName ] “=” username’.
 		 *
-		 *	serviceName: The symbolic name of the bundle which provides this service.
+		 *	serviceName: £The symbolic name of the bundle which provides this service.
 		 *	subServiceName: The value of the passed attribute with the ResourceResolverFactory.SUBSERVICE key.
 		 *	username: The username of the user which should be mapped with this service.
-    	 * 
-    	 * 아래는 Sample 에 적용된 config
-    	 * config path : /apps/adam/config/org.apache.sling.serviceusermapping.impl.ServiceUserMapperImpl.config
-    	 * config mapping : com.amorepacific.adam.commons:writeService=admin        //admin 으로 설정하 현재 서비스는 모든 권한을 가진다.
     	 * 
     	 */
         Map<String, Object> param = new HashMap<String, Object>();
@@ -55,18 +51,8 @@ public class SampleResourceWriter{
         try {
             resolver = resolverFactory.getServiceResourceResolver(param);
             logger.info(resolver.getUserID());
-            Resource res = resolver.getResource("/apps/adam/content/main/jcr:content");
-            ValueMap readMap = res.getValueMap();
-            logger.info(readMap.get("jcr:primaryType", ""));
-            ModifiableValueMap modMap = res.adaptTo(ModifiableValueMap.class);
-            if(modMap != null){
-                modMap.put("writer", "success");
-                resolver.commit();
-                logger.info("Successfully saved");
-            }
+            
         } catch (LoginException e) {
-            logger.error("LoginException",e);
-        } catch (PersistenceException e) {
             logger.error("LoginException",e);
         }finally{
             if(resolver != null && resolver.isLive()){
